7bfc18a20a59c79f3ff40375ddf93b2e26309caa
Himstar <issiaka_a@live.fr>
2018-12-28 08:06:28 +0100

    Add cors middleware

--- src/app.js
+++ src/app.js
@@ -14,6 +14,19 @@ connect();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
+
 app.use(passport.initialize());
 configJWTStrategy();
 
